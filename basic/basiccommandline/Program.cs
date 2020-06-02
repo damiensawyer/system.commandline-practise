2a06e9f7e77528c9c33bb794206d458621b41a8c
Damien Sawyer <damiensawyer@gmail.com>
2020-06-02 08:42:16 +0100

    notes

--- basic/basiccommandline/Program.cs
+++ basic/basiccommandline/Program.cs
@@ -8,6 +8,15 @@ namespace basiccommandline
 {
     class Program
     {
+        /*
+         To run this you need to reference System.CommandLine.DragonFruit from nuget. Without it, this app won't compile because there is no
+         suitable main(). I'm not exactly sure what that dll is doing to intercept the startup. I downloaded the code from 
+         https://github.com/dotnet/command-line-api/tree/master/src/System.CommandLine.DragonFruit but it wasn't obvious to me exactly 
+         what was doing the interception. 
+
+         */
+
+
         /// <summary>
         /// Run with:
         /// dotnet basic/basiccommandline/bin/Debug/netcoreapp3.1/basiccommandline.dll -h
