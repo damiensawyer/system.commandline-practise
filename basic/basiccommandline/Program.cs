2fddc5eaecba1b76b5d3a1fbd22c5014adef0b5f
Damien Sawyer <damiensawyer@gmail.com>
2020-06-02 08:38:50 +0100

    clean up

--- basic/basiccommandline/Program.cs
+++ basic/basiccommandline/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.Parsing;
 using System.IO;
+using System.Reflection;
 
 namespace basiccommandline
 {
     class Program
     {
-
         /// <summary>
-        /// DragonFruit simple example program
+        /// Run with:
+        /// dotnet basic/basiccommandline/bin/Debug/netcoreapp3.1/basiccommandline.dll -h
+        /// or
+        /// dotnet basic/basiccommandline/bin/Debug/netcoreapp3.1/basiccommandline.dll --int-option 20
         /// </summary>
-        /// <param name="intOption">Pass in a number</param>
+        /// <param name="intOption">This is an integer</param>
+        /// <returns></returns>
         static int Main(int intOption)
         {
             Console.WriteLine($"option {intOption}");
